2e51266fc4e1b5014d1f4d319a1240494ab329bb
Vidhi Khaitan <vidhik2002@gmail.com>
2021-07-16 12:06:50 +0530

    feat: generate block

--- src/library.rs
+++ src/library.rs
@@ -35,3 +35,76 @@ pub struct Chain {
     reward: f32,
     difficulty: u32,
 }
+
+impl Chain {
+    pub fn new(miner_address: String, difficulty: u32) -> Chain {
+        let mut chain = Chain {
+            chain : Vec::new(),
+            current_transaction: Vec::new(),
+            difficulty,
+            miner_address,
+            reward: 100.0,
+        };
+
+        //generation of new block
+        chain.generate_block();
+        //return the newly generated chain
+        chain
+    }
+
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool { //as we want to change the instance
+            self.current_transaction.push(Transaction{ //prevents the original instance from being used when function is called
+                sender,
+                receiver,
+                amount,
+            });
+            //returns true if a new transaction has occured
+            true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap();
+        };
+        Chain::hash(&block.header)
+    }
+
+    pub fb update_difficulty(&mut self,  difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
+
+    pub fb update_reward(&mut self,  reward: u32) -> bool {
+        self.reward = reward;
+        true
+    }
+
+    pub fn generate generate_block(&mut self) -> bool {
+        let header = Blockheader{
+            timestamp: time::now().to_timespec().sec,//converting in secconds
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            difficulty: self.difficulty,
+        }
+        let reward_transaction = Transaction {
+            sender: String::from("Root"),
+            reciever: self.miner_address.clone();
+            amount: self.reward;
+        }
+        let mut block = Block {
+            header,//created above
+            count:0 ,// as no current transactions are occuring
+            transactions: vec![]//empty transcation
+        }
+        block.transactions.push(reward_transaction);
+        block.transactions.append(current_transaction);
+        block.count = block.transactions.len() as u32;
+        block.header.merkle =   Chain::get_merkle(block.transactions.clone());
+        Chain::proof_of_work(&mut block.header);
+        println!("{:?}", &block);
+        self.chain.push(block);
+        true
+    }
+}
